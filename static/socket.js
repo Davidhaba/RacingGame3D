15967c1d57af0dee80e90d46fda5b0c3b384f4c7
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2025-03-26 17:42:59 +0100

    Update socket.js

--- static/socket.js
+++ static/socket.js
@@ -3,6 +3,63 @@ const socket = io();
 let playerId;
 const otherPlayers = {};
 
+const addPlayer = id => {
+    if (id !== playerId && !otherPlayers[id]) {
+        const { mesh, body } = createCar();
+        const text = createTextSprite(id);
+        text.position.set(0, 0, 0);
+        mesh.add(text);
+        otherPlayers[id] = { mesh, body };
+    }
+}, 
+
+createTextSprite = t => {
+    const s = 10, f = 3, 
+          canvas = document.createElement("canvas"),
+          ctx = canvas.getContext("2d"),
+          renderSize = f * s;
+    
+    ctx.font = `Bold ${renderSize}px Arial`;
+    const width = Math.ceil(ctx.measureText(t).width * 1.1),
+          height = Math.ceil(renderSize * 1.5);
+    
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.width = (width/s) + "px";
+    canvas.style.height = (height/s) + "px";
+    
+    ctx.font = `Bold ${renderSize}px Arial`;
+    ctx.fillStyle = "white";
+    ctx.textBaseline = "top";
+    ctx.fillText(t, 0, 0);
+    
+    const texture = new THREE.Texture(canvas);
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    texture.needsUpdate = true;
+    
+    const sprite = new THREE.Sprite(
+        new THREE.SpriteMaterial({ map: texture, transparent: true, depthTest: false })
+    );
+    sprite.scale.set(width/s*0.1, height/s*0.1, 1);
+    return sprite;
+},
+
+updatePlayer = (id, { position, quaternion }) => {
+    if (id === playerId || !otherPlayers[id]) return;
+    otherPlayers[id].body.position.copy(position);
+    otherPlayers[id].body.quaternion.copy(quaternion);
+    otherPlayers[id].mesh.position.copy(position);
+    otherPlayers[id].mesh.quaternion.copy(quaternion);
+},
+
+removePlayer = id => {
+    if (!otherPlayers[id]) return;
+    scene.remove(otherPlayers[id].mesh);
+    world.removeBody(otherPlayers[id].body);
+    delete otherPlayers[id];
+};
+
 socket.on('init', (data) => {
     playerId = data.id;
     Object.keys(data.players).forEach(id => {
