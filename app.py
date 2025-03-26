c41571d95f31aa09b8c937de92210cc8551a4384
Davidhaba <154765655+Davidhaba@users.noreply.github.com>
2025-03-26 18:06:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request  # Додано імпорт request
+from flask_socketio import SocketIO, emit  # Додано імпорт emit
 import uuid
 
 app = Flask(__name__)
@@ -10,29 +10,50 @@ players = {}
 
 @socketio.on('connect')
 def handle_connect():
+    # Генеруємо унікальний ідентифікатор гравця
     player_id = str(uuid.uuid4())
-    players[player_id] = {'position': [0, 0, 0], 'quaternion': [0, 0, 0, 1]}
+    players[player_id] = {
+        'position': [0, 0, 0],
+        'quaternion': [0, 0, 0, 1]
+    }
     
-    emit('init', {'id': player_id, 'players': players})
+    # Надсилаємо ініціалізаційні дані клієнту
+    emit('init', {
+        'id': player_id,
+        'players': list(players.keys())
+    })
+    
+    # Сповіщаємо інших гравців про нового учасника
     emit('new_player', {'id': player_id}, broadcast=True, include_self=False)
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    player_id = request.sid
+    player_id = request.sid  # Отримуємо ідентифікатор сесії
     if player_id in players:
+        # Видаляємо гравця зі списку
         del players[player_id]
+        # Сповіщаємо про відключення
         emit('player_left', {'id': player_id}, broadcast=True)
 
 @socketio.on('update_state')
 def handle_state_update(data):
     player_id = request.sid
     if player_id in players:
+        # Оновлюємо стан гравця
         players[player_id] = data
-        emit('state_update', {'id': player_id, 'state': data}, broadcast=True, include_self=False)
+        # Розсилаємо оновлення іншим гравцям
+        emit('state_update', {
+            'id': player_id,
+            'state': data
+        }, broadcast=True, include_self=False)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, 
+                host='0.0.0.0', 
+                port=5000, 
+                debug=True, 
+                allow_unsafe_werkzeug=True)
